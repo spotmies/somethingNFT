02143e2524734261eb0905835420574b98c65815
sekharjavvadi <sekharatece@gmail.com>
2022-07-27 13:36:42 +0530

    initial setup completed

---
+++ components/home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div className="home-parent">
+      <div className="home">
+        <div className="dummy" />
+        <p className="head">Something</p>
+        <div className="icon-links">
+          <img src="/images/e1.png" />
+          <img src="/images/open.png" />
+          <img src="/images/twit.png" />
+        </div>
+      </div>
+    </div>
+  );
+}
