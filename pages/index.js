1fc9b7539622f2dd9ae83aa15da5872aa1ee1133
sekharjavvadi <sekharatece@gmail.com>
2022-08-04 12:21:47 +0530

    google analytics implemented

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import HomePage from "../components/home";
-
+import ReactGA from "react-ga";
+const TRACKING_ID = "UA-230631230-3"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 export default function Home() {
   return (
     <>
